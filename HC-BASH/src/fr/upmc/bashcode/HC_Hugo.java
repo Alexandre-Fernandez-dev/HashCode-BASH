94a89a7b5b1901750c9049d0f04508ee71834c83
HOURCADE <3601334@ppti-14-502-06.ufr-info-p6.jussieu.fr>
2017-02-23 22:19:09 +0100

    p

--- HC-BASH/src/fr/upmc/bashcode/HC_Hugo.java
+++ HC-BASH/src/fr/upmc/bashcode/HC_Hugo.java
@@ -1,5 +1,8 @@
 package fr.upmc.bashcode;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -7,12 +10,12 @@ import fr.upmc.bashcode.model.*;
 
 public class HC_Hugo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		Environnement env = new Environnement();
-		GetData.getData(env, "inputs/me_at_the_zoo.in");
+		GetData.getData(env, "inputs/kittens.in");
 		
-	
+	/*
 	
 		for (Cache c:env.caches){
 			HashMap<Video,Float> score=new HashMap<Video,Float>();
@@ -50,21 +53,73 @@ public class HC_Hugo {
 					break;
 			}
 			
+		}*/
+		
+		float factor_req_latency = (float) 10;
+		float factor_taille_vid = (float) 30000;
+		float factor_datacenter_latency = (float) 0.05;
+		
+		for (Cache c:env.caches){
+			HashMap<Video,Float> score=new HashMap<Video,Float>();
+			
+			for(EndPoint e: c.getEndPointsLatency().keySet() ){
+				for (Video v: e.getVideoRequest().keySet()){
+					if (!score.containsKey(v)){
+						score.put(v, (float)0);
+					}
+					
+					
+					score.put(v, score.get(v)
+							+ factor_req_latency * (e.getVideoRequest().get(v)/(c.getEndPointsLatency().get(e)))
+							+ (factor_taille_vid * 1/v.getSize())
+							+ (factor_datacenter_latency * e.getLatency()));
+				}
+			}
+			while(c.getCurrent()<c.getSizeMax()){
+				Video vmax=null;
+				Float max=(float)0;
+				for (Video v:score.keySet()){
+					if(score.get(v)>max){
+						vmax=v;
+						max=score.get(v);
+					}
+				}
+				if (vmax==null)
+					break;
+								
+				if((c.getCurrent()+vmax.getSize()<=c.getSizeMax())&&(!c.getInCacheVideos().contains(vmax))){
+					c.addVideo(vmax);
+					c.setCurrentFilling(c.getCurrent()+vmax.getSize());
+					score.remove(vmax);
+				}
+				else
+					break;
+			}
+			
 		}
+		for (Cache c:env.caches){
+			c.optimizeCache();
+		}
+		
+		
+		File f = new File("kittensAlex.txt");
+		FileWriter fw = new FileWriter(f);
+		
 		ArrayList<Cache> list_caches=new ArrayList<Cache>();
 		for (Cache c:env.caches){
 			if (!c.getInCacheVideos().isEmpty())
 				list_caches.add(c);
 		}
-		
-		System.out.println(list_caches.size());
+
+		fw.write(list_caches.size()+ "\n");
 		for(Cache c : list_caches){
-			System.out.print(env.caches.indexOf(c) + " ");
+			fw.write(env.caches.indexOf(c) + " ");
 			for(Video v : c.getInCacheVideos()){
-				System.out.print(env.videos.indexOf(v) + " ");
+				fw.write(env.videos.indexOf(v) + " ");
 			}
-			System.out.print("\n");
+			fw.write("\n");
 		}
+		fw.close();
 	}
 	
 }
--- HC-BASH/src/fr/upmc/bashcode/model/Cache.java
+++ HC-BASH/src/fr/upmc/bashcode/model/Cache.java
@@ -42,4 +42,84 @@ public class Cache {
     	inCacheVideos.add(v);
     }
     
+    public void optimizeCache(){
+        HashMap<Video, Integer> totalVideoDataSize = new HashMap<Video, Integer>();
+        
+        HashMap<Video, ArrayList<EndPoint>> epRequiringVideo = new HashMap<Video, ArrayList<EndPoint>>();
+        
+        for(Video v : inCacheVideos) {
+            epRequiringVideo.put(v, new ArrayList<EndPoint>()); 
+            for(EndPoint e : endPointsLatency.keySet()) {
+                if(e.getVideoRequest().keySet().contains(v)) {
+                    epRequiringVideo.get(v).add(e);
+                    if(totalVideoDataSize.get(v) != null)
+                        totalVideoDataSize.put(v, totalVideoDataSize.get(v) + v.getSize()*e.getVideoRequest().get(v));
+                    else
+                        totalVideoDataSize.put(v, v.getSize()*e.getVideoRequest().get(v));
+                }
+            }
+        
+        }
+        
+        Video lowestVideo = null;
+        int lowestSize = 0;
+
+        for(Video v : totalVideoDataSize.keySet()) {
+            if(lowestVideo == null || totalVideoDataSize.get(v)<lowestSize) {
+                lowestVideo=v;
+                
+                lowestSize = totalVideoDataSize.get(v);
+            }
+        }
+
+        HashMap<Video, Integer> videosNotInCache = new HashMap<Video, Integer>();
+        for(EndPoint e : endPointsLatency.keySet()) {
+            for(Video v : e.getVideoRequest().keySet()) {
+                if(!inCacheVideos.contains(v)) {
+                    if(videosNotInCache.containsKey(v))
+                        videosNotInCache.put(v, videosNotInCache.get(v) + v.getSize()*e.getVideoRequest().get(v));
+                    else
+                        videosNotInCache.put(v, v.getSize()*e.getVideoRequest().get(v));
+                }
+            }
+        }
+
+        boolean cont = true;
+        while(cont) {
+            cont = false;
+            Video replacement = null;
+            for(Video v : videosNotInCache.keySet()) {
+                if(videosNotInCache.get(v) > lowestSize) {
+                	if(this.currentFilling - lowestVideo.getSize() + v.getSize() < sizeMax) {
+                    	System.err.println("avant " + lowestSize + " après " + videosNotInCache.get(v));
+	                    replacement = v;
+	                    break;
+                	} else {
+                    	//System.err.println("Va dépasser");
+                	}
+                }
+            }
+
+            if(replacement != null) {
+            	System.err.println("J AMELIORE");
+                cont = true;
+                inCacheVideos.remove(lowestVideo);
+                inCacheVideos.add(replacement);
+                totalVideoDataSize.put(replacement, videosNotInCache.get(replacement));
+                videosNotInCache.remove(replacement);
+                totalVideoDataSize.remove(lowestVideo);
+                this.currentFilling -= lowestVideo.getSize();
+                this.currentFilling += replacement.getSize();
+                if(this.currentFilling>this.sizeMax)
+					try {
+						throw new Exception("Bordel");
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+            }
+        }
+
+         
+    }
+    
 }
