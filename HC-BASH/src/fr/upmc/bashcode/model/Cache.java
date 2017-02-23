7f6f036ba47ea884ffe6aed43929330c20f1d6ba
Alexandre Fernandez <alexandre.fernandez@etu.u-pec.fr>
2017-02-23 19:48:20 +0100

    Pouet

--- HC-BASH/src/fr/upmc/bashcode/model/Cache.java
+++ HC-BASH/src/fr/upmc/bashcode/model/Cache.java
@@ -6,7 +6,7 @@ public class Cache {
     
     private int sizeMax;
     
-    private HashMap<EndPoint, Integer> endPointsLatency;
+    private HashMap<EndPoint, Integer> endPointsLatency = new HashMap<EndPoint,Integer>;
 
     public Cache(int sizeMax) {
         this.sizeMax = sizeMax;
