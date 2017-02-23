459386e72c30c10466cc9dd4df3f49ceae8e3018
Alexandre Fernandez <alexandre.fernandez@etu.u-pec.fr>
2017-02-23 20:06:00 +0100

    Environnement

---
+++ HC-BASH/src/fr/upmc/bashcode/model/Environnement.java
@@ -0,0 +1,12 @@
+package fr.upmc.bashcode.model;
+
+import java.util.ArrayList;
+
+public class Environnement {
+   
+    public ArrayList<EndPoint> endPoints;
+    public ArrayList<Cache> caches; 
+
+    
+    
+}
