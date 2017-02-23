05705a766d3279058d9fbedf3d49060703fb52e3
HOURCADE <3601334@ppti-14-502-06.ufr-info-p6.jussieu.fr>
2017-02-23 19:50:02 +0100

    hugo

--- HC-BASH/src/fr/upmc/bashcode/HC_Hugo.java
+++ HC-BASH/src/fr/upmc/bashcode/HC_Hugo.java
@@ -1,9 +1,29 @@
 package fr.upmc.bashcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.xml.ws.Endpoint;
+
+import fr.upmc.bashcode.model.*;
+
 public class HC_Hugo {
 
 	public static void main(String[] args) {
-		
+		ArrayList<Cache> listCache=new ArrayList<Cache>();
+		for (Cache c:listCache){
+			HashMap<Video,Float> score=new HashMap<Video,Float>();
+			for(Endpoint e: c.getEndpointLatency().getKeySet() ){
+				for (Video v: e.getvideoRequest().getKeySet()){
+					if (!score.contains(v)){
+						score.put(v, 0);
+					}
+					score.put(v, score.get(v)+(e.getvideoRequest().get(v)/(e.getCachesLatency().get(c));
+				}
+			}
+			
+			
+		}
 	}
 	
 }
