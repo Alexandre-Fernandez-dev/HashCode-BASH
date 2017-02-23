879794c1b0a58bd4efedfe4116f2337906b5ead0
HOURCADE <3601334@ppti-14-502-06.ufr-info-p6.jussieu.fr>
2017-02-23 20:58:37 +0100

    Hugo màj

--- HC-BASH/src/fr/upmc/bashcode/HC_Hugo.java
+++ HC-BASH/src/fr/upmc/bashcode/HC_Hugo.java
@@ -3,27 +3,56 @@ package fr.upmc.bashcode;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import javax.xml.ws.Endpoint;
-
 import fr.upmc.bashcode.model.*;
 
 public class HC_Hugo {
 
 	public static void main(String[] args) {
-		ArrayList<Cache> listCache=new ArrayList<Cache>();
-		for (Cache c:listCache){
+		
+		Environnement env = new Environnement();
+		GetData.getData(env, "inputs/me_at_the_zoo.in");
+		
+	
+	
+		for (Cache c:env.caches){
 			HashMap<Video,Float> score=new HashMap<Video,Float>();
-			for(Endpoint e: c.getEndpointLatency().getKeySet() ){
-				for (Video v: e.getvideoRequest().getKeySet()){
-					if (!score.contains(v)){
-						score.put(v, 0);
+			
+			for(EndPoint e: c.getEndPointsLatency().keySet() ){
+				for (Video v: e.getVideoRequest().keySet()){
+					if (!score.containsKey(v)){
+						score.put(v, (float)0);
 					}
-					score.put(v, score.get(v)+(e.getvideoRequest().get(v)/(e.getCachesLatency().get(c));
+					score.put(v, score.get(v)
+							+(e.getVideoRequest().get(v)
+									/(c.getEndPointsLatency().get(e)
+											*v.getSize())
+											*e.getLatency()));
 				}
 			}
-			
+			while(c.getCurrent()<c.getSizeMax()){
+				Video vmax=null;
+				Float max=(float)0;
+				for (Video v:score.keySet()){
+					if(score.get(v)>max){
+						vmax=v;
+						max=score.get(v);
+					}
+				}
+				if (vmax==null)
+					break;
+								
+				if((c.getCurrent()+vmax.getSize()<=c.getSizeMax())&&(!c.getInCacheVideos().contains(vmax))){
+					c.addVideo(vmax);
+					c.setCurrentFilling(c.getCurrent()+vmax.getSize());
+					score.remove(vmax);
+				}
+				else
+					break;
+			}
 			
 		}
+		
+		
 	}
 	
 }
